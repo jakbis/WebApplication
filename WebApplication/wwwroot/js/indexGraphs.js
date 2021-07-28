61ebce1849bb6870fc37cf079cfcf9e076cc26f7
yakirbi <theybi12@gmail.com>
2021-07-28 12:12:07 +0300

    fixed some things

---
+++ WebApplication/wwwroot/js/indexGraphs.js
@@ -0,0 +1,23 @@
+﻿var Sweets = {
+    x: [2018, 2019, 2020, 2021, 2022,2023,2024],
+    y: [18, 18, 28, 32],
+    type: 'scatter'
+};
+
+
+var Salty = {
+    x: [2018, 2019, 2020, 2021, 2022, 2023, 2024],
+    y: [16, 20, 25, 30],
+    type: 'scatter'
+};
+
+
+var Cakes = {
+    x: [2018, 2019, 2020, 2021, 2022, 2023, 2024],
+    y: [10, 20, 16, 25],
+    type: 'scatter'
+};
+
+var data = [Sweets, Salty, Cakes];
+
+Plotly.newPlot('statistic', data);
---
+++ WebApplication/wwwroot/js/logged.js
@@ -0,0 +1,7 @@
+﻿
+
+//$ means as soon the page is ready
+//then you run this function.
+
+//i want you to catch the event of the click with the id "newAuthor"
+//and define the event "click"
